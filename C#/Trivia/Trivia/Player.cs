af02d7be5974fe07e1cae73ff46d8b2c73a78f08
Romain Plassard <Romain Plassard>
2016-03-17 18:01:47 +0100

    modif 17/03/2016

--- C#/Trivia/Trivia/Player.cs
+++ C#/Trivia/Trivia/Player.cs
@@ -1,12 +1,39 @@
+using System.Collections.Generic;
+
 namespace UglyTrivia
 {
     internal class Player
     {
-        public Player(string playerName)
+        public Player(string playerName, string purse, string place)
         {
             Name = playerName;
+            this.Purse = purse;
+            Place = place;
         }
 
         public string Name { get; private set; }
+        public string Purse { get; private set; }
+        public string Place { get; private set; }
+
+        public class Players
+        {
+            List<Player> players = new List<Player>();
+
+            public List<Player> add(Player player)
+            {
+                players.Add(player);
+                return players;
+            }
+
+            public Player Player(int numPlayer)
+            {
+                return players[numPlayer];
+            }
+
+            public int Count()
+            {
+                return players.Count;
+            }
+        }
     }
 }
